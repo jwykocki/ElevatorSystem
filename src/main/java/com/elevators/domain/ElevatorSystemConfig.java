529fe79a4b424f309d5ec1955bc13514a5b6266f
Kuba <jakub.wykocki@wp.pl>
2024-03-20 13:14:53 +0100

    cleaning and formatting files

--- src/main/java/com/elevators/domain/ElevatorSystemConfig.java
+++ src/main/java/com/elevators/domain/ElevatorSystemConfig.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "elevator.system")
